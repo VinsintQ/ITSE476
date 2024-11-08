4a02af970eeb7ed533536566b64d890f8a2fd6c9
Mueen <hejiy2002@gmail.com>
2024-11-08 18:40:53 +0300

    adding some highlights

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,8 @@
 // src/components/Dashboard.jsx
 
+import { FaFileAlt, FaStar, FaThumbsUp } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import '../../Highlights.css';
 const Dashboard = () => {
     return (
       <main>
@@ -14,6 +17,22 @@ const Dashboard = () => {
           <li>View your progress</li>
           <li>calculate bmi </li>
         </ul>
+        <div className="highlights-container">
+      
+      <div className="highlight-item">
+        <div className="icon-circle"><FaStar /></div>
+        <h2>Fitness Log Conference 2024</h2>
+        <p>December 10 – 12 in Manama. We'll have workshops, discussions and lots of fun!</p>
+        <a href="#join" className="highlight-link">Join us there</a>
+      </div>
+      <div className="highlight-item">
+        <div className="icon-circle"><FaThumbsUp /></div>
+        <h2>Fantastic People</h2>
+        <p>Fitness Log is about more than software. It’s about people, culture, creation, sharing and collaboration.</p>
+        <a href="#join-today" className="highlight-link"><Link to="/Joinus"> Join Us today</Link></a>
+      </div>
+    </div>
+        
       </main>
     );
   };
