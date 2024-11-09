208adffcdc3523dec045bd2754af609e151b840f
ali-alaalem <Meemo2002a@gmail.com>
2024-11-09 13:16:15 +0300

    Adding the link for downloading least version of the system in the announcment page and modifing the about us page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,36 +1,62 @@
 // src/components/Dashboard.jsx
 
 const About = () => {
-    return (
-      <main>
-        <h1>Our Mission</h1>
-        <p>
-        FitnessLog is a free, open-source platform dedicated to making fitness logging simple, accessible, and customizable for everyone. Our mission is to provide users with an intuitive tool to track, edit, and analyze their workouts while calculating calories burned and monitoring progress over time.
+  return (
+    <main>
+      <h1>Our Mission</h1>
+      <p>
+        FitnessLog is a free, open-source platform dedicated to making fitness
+        logging simple, accessible, and customizable for everyone. Our mission
+        is to provide users with an intuitive tool to track, edit, and analyze
+        their workouts while calculating calories burned and monitoring progress
+        over time. We believe in promoting healthy, active lifestyles through
+        transparency and community-driven development. By making FitTrack open
+        and accessible, we aim to empower fitness enthusiasts, trainers, and
+        developers alike to contribute to and innovate within the fitness
+        community.
+      </p>
+      <h3>Our Team </h3>
+      <ul>
+        <li>
+          Mueen <p>Developer</p>
+        </li>
+        <li>
+          Ali <p>Developer</p>
+        </li>
+        <li>
+          Mujtaba <p>Developer</p>
+        </li>
+      </ul>
 
-         We believe in promoting healthy, active lifestyles through transparency and community-driven development. By making FitTrack open and accessible, we aim to empower fitness enthusiasts, trainers, and developers alike to contribute to and innovate within the fitness community.
-        </p>
-         <h3>Our Team </h3>
-         <ul>
-            <li>Mueen <p>Developer</p></li>
-            </ul>
+      <h2>License</h2>
+      <h3>GNU General Public License</h3>
+      <p>
+        Fitness Log is released under the GNU General Public License v3.0. This
+        license ensures that the software remains free and open-source, allowing
+        users to use, modify, and distribute the code.
+      </p>
 
-        <h2>License</h2> 
-        <h3>GNU General Public License</h3>   
-       <p>Fitness Log is released under the GNU General Public License v3.0.
-         This license ensures that the software remains free and open-source, 
-         allowing users to use, modify, and distribute the code.</p> 
-         
-    <ul><li>You are free to use, modify, and distribute this software.</li>
-<li>
-If you share modified versions, your changes must also be made available under this same license.
- Please include a copy of the license and the copyright notice with any distribution.
-</li>
-<li>This software comes with no warranty, as permitted by applicable law.</li><li>
-<h4> <a href="https://github.com/VinsintQ/ITSE476?tab=GPL-3.0-1-ov-file">Full license</a></h4></li></ul>
+      <ul>
+        <li>You are free to use, modify, and distribute this software.</li>
+        <li>
+          If you share modified versions, your changes must also be made
+          available under this same license. Please include a copy of the
+          license and the copyright notice with any distribution.
+        </li>
+        <li>
+          This software comes with no warranty, as permitted by applicable law.
+        </li>
+        <li>
+          <h4>
+            {" "}
+            <a href="https://github.com/VinsintQ/ITSE476?tab=GPL-3.0-1-ov-file">
+              Full license
+            </a>
+          </h4>
+        </li>
+      </ul>
+    </main>
+  );
+};
 
-
-      </main>
-    );
-  };
-  
-  export default About;
+export default About;
